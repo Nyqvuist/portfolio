a207009c2b1f2de1e8e88643d2d0e375fe6aa3a3
Hassan Shirazi <shiraziahassan@gmail.com>
2024-09-27 18:01:40 -0400

    fixed grid of modal

--- src/components/Chest.tsx
+++ src/components/Chest.tsx
@@ -7,6 +7,9 @@ import { useDisclosure } from '@mantine/hooks';
 import { Modal } from '@mantine/core';
 import { Text } from "@mantine/core";
 import box from "../assets/HWP_Ind_Inv_Slot_1920x1080.png";
+import { Grid } from '@mantine/core';
+import textfield from "../assets/HWP_Inner_Inv_w_Textbox_1920x1080.png";
+import divider from "../assets/HWP_Inv_Divider_1920x1080.png";
 
 export default function Chest() {
   const [chest, setChest] = React.useState(false);
@@ -18,14 +21,46 @@ export default function Chest() {
         className="modal"
         opened={opened}
         onClose={close}
-        size={"65%"}
+        size={"100%"}
         withCloseButton={false}
       >
-        <Image className="inv_slot" src={box} fit="fill" h={120} w={120} />
-        <Text>what the hell is going on</Text>
-        <Text>what the hell is going on</Text>
-        <Text>what the hell is going on</Text>
-        <Text>what the hell is going on</Text>
+        
+        <Grid align="stretch">
+          <Grid.Col span={6} className="colone">
+          <Grid gutter={"md"}>
+            <Grid.Col span={3}>
+            <Image className="inv_slot" src={box} fit="fill" h={150} w={150} />
+            <Image className="inv_slot" src={box} fit="fill" h={150} w={150} />
+            <Image className="inv_slot" src={box} fit="fill" h={150} w={150} />
+            <Image className="inv_slot" src={box} fit="fill" h={150} w={150} />
+            </Grid.Col>
+            <Grid.Col span={3}>
+            <Image className="inv_slot" src={box} fit="fill" h={150} w={150} />
+            <Image className="inv_slot" src={box} fit="fill" h={150} w={150} />
+            <Image className="inv_slot" src={box} fit="fill" h={150} w={150} />
+            <Image className="inv_slot" src={box} fit="fill" h={150} w={150} />
+            </Grid.Col>
+            <Grid.Col span={3}>
+            <Image className="inv_slot" src={box} fit="fill" h={150} w={150} />
+          <Image className="inv_slot" src={box} fit="fill" h={150} w={150} />
+          <Image className="inv_slot" src={box} fit="fill" h={150} w={150} />
+          <Image className="inv_slot" src={box} fit="fill" h={150} w={150} />
+            </Grid.Col>
+            <Grid.Col span={3}>
+            <Image className="inv_slot" src={box} fit="fill" h={150} w={150} />
+          <Image className="inv_slot" src={box} fit="fill" h={150} w={150} />
+          <Image className="inv_slot" src={box} fit="fill" h={150} w={150} />
+          <Image className="inv_slot" src={box} fit="fill" h={150} w={150} />
+            </Grid.Col>
+          </Grid>
+          </Grid.Col>
+        <Grid.Col span={6}>
+          <Image className="inv_description" src={textfield} fit="fill" h={600} w={950} />
+          <div className="description_div">
+          <Text >HELLLO</Text>
+          </div>
+        </Grid.Col>
+        </Grid>
       </Modal>
       <div className="button-div" onClick={open}>
         <Image
